5576433924a1be6b0c9052e2279ebd498b680311
kaleab-desciple <136029756+kaleab-desciple@users.noreply.github.com>
2023-11-29 00:33:16 +0300

    Update Courses.java

--- Courses.java
+++ Courses.java
@@ -1,13 +1,16 @@
 package sms_updated;
 
+    // class with int variable and Array of StudCourse objects
 public class Courses {
 
     int rollNumber;
-    StudCourse [] sc;
 
+    // creating an instance Array of  class StudCourse
+    StudCourse [] sc;    
+
+    // course object constructor 
     public Courses(int rollNumber, StudCourse[] sc ){
         this.rollNumber=rollNumber;
-
         this.sc=sc;
     }
 }
